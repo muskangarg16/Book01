1edce59faba0ca38dc4a697eab8c7d35ea450277
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2019-09-21 09:47:53 +0530

    declaration of variables

--- src/Question3/Solution.java
+++ src/Question3/Solution.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         final Scanner=new Scanner(System.in);
-        int n=in.nextInt();
-        if(n==0)
+        int n = in.nextInt();
+        if (n == 0)
             System.out.println("N/A");
-        else{
-            Book[] books=new Book[n];
+        else {
+            Book[] books = new Book[n];
             in.nextLine().trim();
-            for(int i=0;i<n;i++){
-                String bookName=in.nextLine().trim();
-                String isbn=in.nextLine().trim();
-                books[i]=new Book(bookName,authorName,isbn);
+            for (int i = 0; i < n; i++) {
+                String bookName = in.nextLine().trim();
+                String isbn = in.nextLine().trim();
+                books[i] = new Book(bookName, authorName, isbn);
             }
-            for(int j=0;j<books.length;j++){
+            for (int j = 0; j < books.length; j++) {
                 System.out.println(books[j]);
 
             }
@@ -25,5 +25,7 @@ public class Solution {
     }
 }
 class Book{
-
+    private String bookName;
+    private String authorName;
+    private String isbn;
 }
